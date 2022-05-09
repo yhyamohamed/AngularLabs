96c4890162ae81344bf07c96034d8e80a4357d07
yhyamohamed <62445980+yhyamohamed@users.noreply.github.com>
2022-05-09 14:34:16 +0200

    components based page

---
+++ Lab1/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { DepartmentComponent } from './department/department.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LeftMenueComponent } from './left-menue/left-menue.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentComponent,
+    DepartmentComponent,
+    NavbarComponent,
+    LeftMenueComponent,
+    ContentComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Lab1/src/app/left-menue/left-menue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-menue',
+  templateUrl: './left-menue.component.html',
+  styleUrls: ['./left-menue.component.css']
+})
+export class LeftMenueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
